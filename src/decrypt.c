0cf7c6742c4d0330eced491c6e1b866ea7b934fc
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-06-02 01:38:15 +0200

    Added declarations for encrypt/decrypt functions.

---
+++ src/decrypt.c
@@ -0,0 +1,7 @@
+#include "cecies/decrypt.h"
+
+int cecies_decrypt(const unsigned char* encrypted_data, size_t encrypted_data_length, const unsigned char* private_key, size_t private_key_length, const unsigned char* private_key_passphrase, size_t private_key_passphrase_length, unsigned char* output, size_t output_bufsize, size_t* output_length)
+
+{
+    // TODO: implement ASAP
+}
