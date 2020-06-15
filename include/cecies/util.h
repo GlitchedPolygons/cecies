91c5e6bc2eba7f4a6e9e0c4174207c5ffaaf88f8
Glitched Polygons <37942667+GlitchedPolygons@users.noreply.github.com>
2020-06-15 12:58:47 +0200

    Update util.h

--- include/cecies/util.h
+++ include/cecies/util.h
@@ -160,21 +160,21 @@ static inline unsigned long long int cecies_get_random_big_integer()
 }
 
 /**
- * (Tries to) read from <c>/dev/urandom</c> (or Windows equivalent, yea..) filling the given \p output_buffer with \p output_buffer_size random bytes.
+ * (Tries to) read from <c>/dev/urandom</c> (or Windows equivalent, yeah...) filling the given \p output_buffer with \p output_buffer_size random bytes.
  * @param output_buffer Where to write the random bytes into.
  * @param output_buffer_size How many random bytes to write into \p output_buffer
  */
 static inline void cecies_dev_urandom(unsigned char* output_buffer, const size_t output_buffer_size)
 {
 #ifdef _WIN32
-    BCryptGenRandom(NULL, output_buffer, output_buffer_size, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+        BCryptGenRandom(NULL, output_buffer, output_buffer_size, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
 #else
-    FILE* rnd = fopen("/dev/urandom", "r");
-    if (rnd != NULL && output_buffer != NULL)
-    {
-        fread(output_buffer, sizeof(unsigned char), output_buffer_size, rnd);
-        fclose(rnd);
-    }
+        FILE* rnd = fopen("/dev/urandom", "r");
+        if (rnd != NULL)
+        {
+            fread(output_buffer, sizeof(unsigned char), output_buffer_size, rnd);
+            fclose(rnd);
+        }
 #endif
 }
 
