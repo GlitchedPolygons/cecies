d6706cf6d8c4a18589bb8f032791d09ebf9cf77e
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-09-20 18:41:31 +0200

    Improved build.bat (replaces msbuild calls with cmake --build to allow running via standard cmd).

--- build.bat
+++ build.bat
@@ -4,13 +4,12 @@ SET out="%repo%\build-msvc"
 if exist %out% ( rd /s /q %out% )
 mkdir %out% && cd %out%
 cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_ENABLE_TESTS=Off -DCECIES_ENABLE_PROGRAMS=On -DCMAKE_BUILD_TYPE=Release ..
-msbuild cecies.vcxproj /p:configuration=release
-msbuild programs\ALL_BUILD.vcxproj /p:configuration=release
+cmake --build . --config Release
 mkdir include
 xcopy ..\include .\include
 mkdir dll && cd dll
 cmake -DBUILD_SHARED_LIBS=On -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_BUILD_DLL=On -DCECIES_ENABLE_PROGRAMS=Off -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ..\..
-msbuild cecies.vcxproj /p:configuration=release
+cmake --build . --config Release
 cd ..
 tar -czvf cecies.tar.gz Release\* programs\Release\*.exe dll\Release\*.dll dll\Release\*.lib dll\Release\*.exp
 cd %RET%
