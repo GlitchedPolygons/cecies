d85ddc5343309fc5c91a26960b8c7e11622c2207
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-09-20 22:23:22 +0200

    Modified build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-if [ "$(whoami)" == "root" ]; then
+if [ "$(whoami)" = "root" ]; then
   echo "  Please don't run as root/using sudo..."
   exit
 fi
@@ -23,14 +23,19 @@ REPO=$(dirname "$0")
 rm -rf "$REPO"/out
 rm -rf "$REPO"/build
 mkdir -p "$REPO"/build/shared && cd "$REPO"/build || exit
-cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_ENABLE_PROGRAMS=On -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ..
-make
+cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_ENABLE_PROGRAMS=On -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release .. || exit
+make || exit
 cp -r ../include ./
-cd "$REPO"/build/shared || exit
-cmake -DBUILD_SHARED_LIBS=On -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_BUILD_DLL=On -DCECIES_ENABLE_PROGRAMS=Off -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ../..
-make
-cd "$REPO"/build || exit
-tar -czvf cecies.tar.gz *.lib *.a programs/*_keygen programs/*_encrypt programs/*_decrypt programs/*_sign programs/*_verify shared/*.dll shared/*.dylib shared/*.so include/**/*
+cd shared || exit
+cmake -DBUILD_SHARED_LIBS=On -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_BUILD_DLL=On -DCECIES_ENABLE_PROGRAMS=Off -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ../.. || exit
+make || exit
+cd .. || exit
+mkdir static || exit
+cd static || exit
+cmake -DBUILD_SHARED_LIBS=Off -DUSE_SHARED_MBEDTLS_LIBRARY=Off -DCECIES_BUILD_DLL=Off -DCECIES_ENABLE_PROGRAMS=Off -DCECIES_ENABLE_TESTS=Off -DCMAKE_BUILD_TYPE=Release ../.. || exit
+make || exit
+cd .. || exit
+tar -czvf cecies.tar.gz programs/*_keygen programs/*_encrypt programs/*_decrypt programs/*_sign programs/*_verify shared/*.dll shared/*.dylib shared/*.so static/*.a static/*.lib include/**/*
 cd "$REPO" || exit
 echo "  Done. Exported build into $REPO/build"
 echo "  Check out the cecies.tar.gz file in there! "
